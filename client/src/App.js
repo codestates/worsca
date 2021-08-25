8ed4396cf1d78efaad95e66ed3aeebd8bef15ce7
jmean12 <ooyhy123aa@gmail.com>
2021-08-25 17:41:58 +0900

    C_signup1.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    list-style: none;
+    text-decoration: none;
+  }
+`;
 
 function App() {
-	return <div className="App">test</div>;
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/"></Route>
+				<Route exact path="/map"></Route>
+				<Route exact path="/login"></Route>
+				<Route exact path="/signup"></Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
