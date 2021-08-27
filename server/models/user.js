260729c9abd796c54bb1f38f7ed213a644d47ab8
jihunv8 <jihunv8@gmail.com>
2021-08-27 21:36:25 +0900

    Server Add model Review. DB reviews 테이블 구현

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			// define association here
+			models.User.hasMany(models.Review, {
+				foreignKey: 'user_id',
+				sourceKey: 'id'
+			});
 		}
 	}
 	User.init(
