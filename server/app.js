22a38865d11337dca0e8a93990a9cb8dacf223b0
jihunv8 <jihunv8@gmail.com>
2021-08-27 19:06:26 +0900

    Server Correct app.js, config.js등 sequelize 설정 .env로전부 관리, 요청확인로그 추가

--- server/app.js
+++ server/app.js
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(express.json());
 
+//요청 확인 로그 미들웨어
+app.use((req, res, next) => {
+	console.log(req.method, req.url);
+	console.log(req.body);
+	console.log("-----------------------");
+	next();
+});
+
 app.use("/users", signupRouter);
 
 app.get("/", (req, res) => {
