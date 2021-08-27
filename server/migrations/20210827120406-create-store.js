33d27f80710fcece52a8bdbfc76c4fe9b38a887c
jihunv8 <jihunv8@gmail.com>
2021-08-27 21:21:15 +0900

    Sever Add model store. 데이터베이스 stores테이블 구현

---
+++ server/migrations/20210827120406-create-store.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Stores', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      store_id: {
+        type: Sequelize.INTEGER
+      },
+      owner_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'set null'
+      },
+      number: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      homepage: {
+        type: Sequelize.STRING
+      },
+      open: {
+        type: Sequelize.DATE
+      },
+      close: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Stores');
+  }
+};
